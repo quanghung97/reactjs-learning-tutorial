4b8eb3e5837794571cfde543c566a713d08537d9
quanghung97 <hungcan1997@gmail.com>
2019-05-06 15:17:01 +0700

    lession 2 components

---
+++ lession02-component/src/components/Product.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Product() {
+  return (
+    <div classname="card">
+      <img src="..." classname="card-img-top" alt="..." />
+        <div classname="card-body">
+          <h5 classname="card-title">Card title</h5>
+          <p classname="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="asd" classname="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+  );
+}
+
+export default Product;
